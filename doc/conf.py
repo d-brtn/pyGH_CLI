6ed9c04c0d056947d3c3955258bfcdcfca5b9224
David Burton <20693977+d-brtn@users.noreply.github.com>
2024-12-16 00:44:48 +0000

    modified sys.path insertion with ./doc/conf.py

--- doc/conf.py
+++ doc/conf.py
@@ -31,7 +31,8 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # Add the ../src directory to sys.path
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../src'))
+from pathlib import Path
+sys.path.insert(0, str(Path('..", pyghcli').resolve()))
 
 print(f"path : {sys.path}")
 # -- Options for HTML output -------------------------------------------------
